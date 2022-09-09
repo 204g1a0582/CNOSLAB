6066b545a46b07dcb62123dc46ac4e0ae2e1d442
204g1a0582 <94512824+204g1a0582@users.noreply.github.com>
2022-09-09 15:21:18 +0530

    FCFS.c file is created

---
+++ FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.Rithika
+   Roll No: 204G1A0582
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 09-09-2022
+   Date of Execution: 09-09-2022
+*/
