a0a3267b1939f3b6dc2b4f26eaa5b7680e99fc60
204g1a0582 <94512824+204g1a0582@users.noreply.github.com>
2022-09-16 15:19:12 +0530

    SJF.c file is created.

---
+++ SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: M.rITHIKA
+   Roll No: 204G1A0582
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
